5b736ab72aa399dc1664556f397ff48f953ea70b
saumyasharma03 <saumya.sharma0915@gmail.com>
2025-04-30 12:17:29 +0530

    httpagent removal from frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
-import http from "http";
+// import http from "http";
 
-const httpAgent = new http.Agent({
-  keepAlive: true,
-  maxSockets: 100, // optional
-});
+// const httpAgent = new http.Agent({
+//   keepAlive: true,
+//   maxSockets: 100, // optional
+// });
 
 const API = axios.create({
   baseURL: "http://localhost:5000/api",
-  httpAgent,
+  
 });
 
 export default API;
